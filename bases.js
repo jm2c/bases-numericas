7a04af4114892baaaa8a2375892a439a7c692321
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2019-01-10 19:46:57 -0600

    First commit

---
+++ bases.js
@@ -0,0 +1,32 @@
+function convert(num, from, to) {
+    return -1;
+}
+
+function add(num1, num2, base) {
+    return -1;
+}
+
+function substract(num1, num2, base) {
+    return -1;
+}
+
+function multiply(num1, num2, base) {
+    return -1;
+}
+
+function quotient(num1, num2, base) {
+    return -1;
+}
+
+function residue(num1, num2, base) {
+    return -1;
+}
+
+module.exports = {
+    convert: convert,
+    add: add,
+    substract: substract,
+    multiply: multiply,
+    quotient: quotient,
+    residue: residue,
+};
