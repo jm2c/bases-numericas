8eb0f7140fe08c058a6169bfd1fd61daf7c552ed
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2019-01-11 22:00:14 -0600

    V1.0

--- bases.js
+++ bases.js
@@ -1,25 +1,64 @@
 function convert(num, from, to) {
-    return -1;
+    if(arguments.length != 3)
+        throw 'Wrong number in parameters';
+    
+    if(from < 2 || from > 36 || to < 2 || to > 36)
+        throw 'Base out of range';
+
+    if(num.startsWith('-')) 
+        throw 'Negative number';
+
+    if(num.indexOf('.') >= 0)
+        throw 'Possible float number';
+    
+    const number = parseInt(num, from);
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numArray = num.toString().split('');
+    
+    if(isNaN(number) || /[^A-Z0-9]/i.test(num))
+        throw 'Wrong argument';
+        
+    for(let i = 0; i < numArray.length; i++){
+        if( chars.indexOf(numArray[i]) >= from )
+            throw 'Wrong formed number';
+    }
+
+    return number.toString(to).toUpperCase();
 }
 
 function add(num1, num2, base) {
-    return -1;
+    const a = parseInt(convert(num1, base, 10));
+    const b = parseInt(convert(num2, base, 10));
+    return convert((a+b).toString(), 10, base);
 }
 
 function substract(num1, num2, base) {
-    return -1;
+    const a = parseInt(convert(num1, base, 10));
+    const b = parseInt(convert(num2, base, 10));
+    return convert((a-b).toString(), 10, base);
 }
 
 function multiply(num1, num2, base) {
-    return -1;
+    const a = parseInt(convert(num1, base, 10));
+    const b = parseInt(convert(num2, base, 10));
+    return convert((a*b).toString(), 10, base);
 }
 
 function quotient(num1, num2, base) {
-    return -1;
+    if(num2 === 0 || parseInt(num2) === 0)
+        throw 'Division by zero';
+    const a = parseInt(convert(num1, base, 10));
+    const b = parseInt(convert(num2, base, 10));
+    const q = Math.floor(a/b);
+    return convert(q.toString(), 10, base);
 }
 
 function residue(num1, num2, base) {
-    return -1;
+    if(num2 === 0 || parseInt(num2) === 0)
+        throw 'Division by zero';
+    const a = parseInt(convert(num1, base, 10));
+    const b = parseInt(convert(num2, base, 10));
+    return convert((a%b).toString(), 10, base);
 }
 
 module.exports = {
--- bases.test.js
+++ bases.test.js
@@ -21,19 +21,27 @@ describe('Conversiones', () => {
         expect(bases.convert('P', 32, 20)).toBe('15');
     });
 
+    test('Letras minúsculas', () => {
+        expect(bases.convert('abc', 16, 16)).toBe('ABC');
+    });
+
     test('Números mal formados', () => {
-        expect(bases.convert('uno', 10, 16)).toThrow();
-        expect(bases.convert('1A', 10, 16)).toThrow();
-        expect(bases.convert('3', 2, 10)).toThrow();
-        expect(bases.convert('-3', 10, 10)).toThrow();
-        expect(bases.convert('2+2', 10, 10)).toThrow();
-        expect(bases.convert('3.5', 10, 10)).toThrow();
+        expect(() => bases.convert('uno', 10, 16)).toThrow();
+        expect(() => bases.convert('1A', 10, 16)).toThrow();
+        expect(() => bases.convert('3', 2, 10)).toThrow();
+        expect(() => bases.convert('-3', 10, 10)).toThrow();
+        expect(() => bases.convert('2+2', 10, 10)).toThrow();
+        expect(() => bases.convert('3.5', 10, 10)).toThrow();
     });
 
     test('Parámetros incorrectos', () => {
-        expect(bases.convert()).toThrow();
-        expect(bases.convert('10')).toThrow();
-        expect(bases.convert('10', 10)).toThrow();
+        expect(() => bases.convert()).toThrow();
+        expect(() => bases.convert('10')).toThrow();
+        expect(() => bases.convert('10', 10)).toThrow();
+        expect(() => bases.convert('10', 1, 10)).toThrow();
+        expect(() => bases.convert('10', 10, 1)).toThrow();
+        expect(() => bases.convert('10', 37, 10)).toThrow();
+        expect(() => bases.convert('10', 10, 37)).toThrow();
     });
 });
 
@@ -61,6 +69,8 @@ describe('Resta', () => {
 // MULTIPLICACIÓN
 describe('Multiplicación', () => {
     test('Multiplicación de dos números', () => {
+        expect(bases.multiply('9', '1', 10)).toBe('9');
+        expect(bases.multiply('9', '0', 10)).toBe('0');
         expect(bases.multiply('9', '5', 10)).toBe('45');
         expect(bases.multiply('7', '7', 8)).toBe('61');
         expect(bases.multiply('C', '4', 16)).toBe('30');
@@ -95,10 +105,13 @@ describe('División y residuo', () => {
 
         expect(bases.quotient('1100', '101', 2)).toBe('10');
         expect(bases.residue('1100', '101', 2)).toBe('10');
+    });
+    
+    test('División por cero', () => {
+        expect(() => bases.quotient('1', '0', 10)).toThrow();
+        expect(() => bases.residue('1', '0', 10)).toThrow();
         
-        expect(bases.quotient('1', '0', 10)).toThrow();
-        expect(bases.residue('1', '0', 10)).toThrow();
-        expect(bases.quotient('1', '0', 5)).toThrow();
-        expect(bases.residue('1', '0', 5)).toThrow();
+        expect(() => bases.quotient('1', '0', 5)).toThrow();
+        expect(() => bases.residue('1', '0', 5)).toThrow();
     });
 });
